07f3c0903869efeddc76518258c79d42f7655eaf
esdmr <esdmr0@gmail.com>
2020-06-25 19:05:10 +0430

    Replace 'TOO DEEP' for unknown

--- src/typedef.ts
+++ src/typedef.ts
@@ -7,7 +7,7 @@ type Tail<T extends readonly unknown[]> =
 type RecursionLimit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 export type GetValue<T, K extends readonly unknown[]> = {
 	done: T,
-	deep: 'TOO DEEP',
+	deep: unknown,
 	recur: GetValue<Index<T, K[0]>, Tail<K>>,
 }[K['length'] extends 0 ? 'done'
 	: K['length'] extends RecursionLimit ? 'recur': 'deep'];
